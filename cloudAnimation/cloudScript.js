725cc3ea3d7b0611f27379f2f15be578d65b8f19
laraib-906 <56772304+laraib-906@users.noreply.github.com>
2020-07-23 22:49:10 +0500

    Add files via upload
    svg animation and mp4 video

---
+++ cloudAnimation/cloudScript.js
@@ -0,0 +1,22 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "Cloud-animated-illustration.json",
+    innerHeight:100
+})
+function show(){
+    var animation=document.getElementById("anim");
+    var animationSetting= animation.style.display="block";
+    var mp4 = document.getElementById("mp4Video");
+    var soundButt = document.getElementById("soundButton");
+    console.log(mp4)
+    if(animationSetting == "block"){
+        animation.style.display="none"
+        mp4.style.display="block"
+        soundButt.style.display="block"
+    }
+
+    
+}
