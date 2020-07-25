3fb0e0ecad50e101f0bc2501194281a97fa94442
laraib-906 <56772304+laraib-906@users.noreply.github.com>
2020-07-25 21:03:03 +0500

    owl

---
+++ javaScript/owlCarouselSetup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        // 500:{
+        //     items:3
+        // },
+        1000:{
+            items:3
+        }
+    }
+})
+function clickslide(){
+    var slide = document.getElementById("slide1")
+    
+}
