321b7d4b91bcd550972c55b0f9a0f5afd8a73955
Ammar <ashabbir197@gmail.com>
2020-07-25 22:46:10 +0500

    owl sample

--- javaScript/owlCarouselSetup.js
+++ javaScript/owlCarouselSetup.js
@@ -2,13 +2,14 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    // autoplay:true,
+    startPosition: 'URLHash',
+    center:true,
+    // autoWidth:true,
     responsive:{
         0:{
             items:1
         },
-        // 500:{
-        //     items:3
-        // },
         1000:{
             items:3
         }
