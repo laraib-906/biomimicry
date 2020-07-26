db60cf0f64b341f7e2736ea891d701cec6b352b7
laraib-906 <56772304+laraib-906@users.noreply.github.com>
2020-07-27 01:15:39 +0500

    squid, shell, beetle animation complete with out text

--- js/ShellScript.js
+++ js/ShellScript.js
@@ -1,33 +1,33 @@
 var animation1 = bodymovin.loadAnimation({
-    container: document.getElementById('beetle1Ani'),
+    container: document.getElementById('shell1Ani'),
     renderer: 'svg',
     loop: true,
     autoplay: false,
-    path: "/biomimicry/media/animations/Beetle/Beetle_01/Beettle_01.json",
+    path: "/biomimicry/media/animations/shell/shell_01/shell_01.json",
     innerHeight:100
 })
 var animation2 = bodymovin.loadAnimation({
-    container: document.getElementById('beetle2Ani'),
+    container: document.getElementById('shell2Ani'),
     renderer: 'svg',
     loop: true,
     autoplay: false,
-    path: "/biomimicry/media/animations/Beetle/Beettle_02/Beetle_02.json",
+    path: "/biomimicry/media/animations/shell/shell_02/shell_02.json",
     innerHeight:100
 })
 var animation3 = bodymovin.loadAnimation({
-    container: document.getElementById('beetle3Ani'),
+    container: document.getElementById('shell3Ani'),
     renderer: 'svg',
     loop: true,
     autoplay: false,
-    path: "/biomimicry/media/animations/Beetle/Beetle_03/Beetle_03.json",
+    path: "/biomimicry/media/animations/shell/shell_03/shell_03.json",
     innerHeight:100
 })
 var animation4 = bodymovin.loadAnimation({
-    container: document.getElementById('beetle4Ani'),
+    container: document.getElementById('shell4Ani'),
     renderer: 'svg',
     loop: true,
     autoplay: false,
-    path: "/biomimicry/media/animations/Beetle/Beetle_04/Beetle_04.json",
+    path: "/biomimicry/media/animations/shell/shell_04/shell_04.json",
     innerHeight:100
 })
 function showcircle(){
@@ -36,12 +36,12 @@ function showcircle(){
 function stopcircle (){
     animation1.pause()
 }
-function showBeetle(){
-    var img= document.getElementById("imageBeetle")
-    var div1=document.getElementById("beetle1Ani")
-    var div2=document.getElementById("beetle2Ani")
-    var div3 = document.getElementById("beetle3Ani")
-    var div4 = document.getElementById("beetle4Ani")
+function showshell(){
+    var img= document.getElementById("imageshell")
+    var div1=document.getElementById("shell1Ani")
+    var div2=document.getElementById("shell2Ani")
+    var div3 = document.getElementById("shell3Ani")
+    var div4 = document.getElementById("shell4Ani")
     if(img.style.display && div1.style.display == "block"){
         img.style.display="none"
         div1.style.display="none"
@@ -64,14 +64,14 @@ function showBeetle(){
     
 }
 
-function hideBeetle(){
-    var img= document.getElementById("imageBeetle")
-    var div1=document.getElementById("beetle1Ani")
+function hideshell(){
+    var img= document.getElementById("imageshell")
+    var div1=document.getElementById("shell1Ani")
     var imgSetting = img.style.display
     var div1Setting = div1.style.display
-    var div2=document.getElementById("beetle2Ani")
-    var div3=document.getElementById("beetle3Ani")
-    var div4=document.getElementById("beetle4Ani")
+    var div2=document.getElementById("shell2Ani")
+    var div3=document.getElementById("shell3Ani")
+    var div4=document.getElementById("shell4Ani")
     if(imgSetting && div1Setting == "none"){
         img.style.display="block"
         div1.style.display="block"
--- js/SquidScript.js
+++ js/SquidScript.js
@@ -1,33 +1,33 @@
 var animation1 = bodymovin.loadAnimation({
-    container: document.getElementById('beetle1Ani'),
+    container: document.getElementById('squid1Ani'),
     renderer: 'svg',
     loop: true,
     autoplay: false,
-    path: "/biomimicry/media/animations/Beetle/Beetle_01/Beettle_01.json",
+    path: "/biomimicry/media/animations/squid/squid_01/squid_01.json",
     innerHeight:100
 })
 var animation2 = bodymovin.loadAnimation({
-    container: document.getElementById('beetle2Ani'),
+    container: document.getElementById('squid2Ani'),
     renderer: 'svg',
     loop: true,
     autoplay: false,
-    path: "/biomimicry/media/animations/Beetle/Beettle_02/Beetle_02.json",
+    path: "/biomimicry/media/animations/squid/squid_02/squid_02.json",
     innerHeight:100
 })
 var animation3 = bodymovin.loadAnimation({
-    container: document.getElementById('beetle3Ani'),
+    container: document.getElementById('squid3Ani'),
     renderer: 'svg',
     loop: true,
     autoplay: false,
-    path: "/biomimicry/media/animations/Beetle/Beetle_03/Beetle_03.json",
+    path: "/biomimicry/media/animations/squid/squid_03/squid_03.json",
     innerHeight:100
 })
 var animation4 = bodymovin.loadAnimation({
-    container: document.getElementById('beetle4Ani'),
+    container: document.getElementById('squid4Ani'),
     renderer: 'svg',
     loop: true,
     autoplay: false,
-    path: "/biomimicry/media/animations/Beetle/Beetle_04/Beetle_04.json",
+    path: "/biomimicry/media/animations/squid/squid_04/squid_04.json",
     innerHeight:100
 })
 function showcircle(){
@@ -36,12 +36,12 @@ function showcircle(){
 function stopcircle (){
     animation1.pause()
 }
-function showBeetle(){
-    var img= document.getElementById("imageBeetle")
-    var div1=document.getElementById("beetle1Ani")
-    var div2=document.getElementById("beetle2Ani")
-    var div3 = document.getElementById("beetle3Ani")
-    var div4 = document.getElementById("beetle4Ani")
+function showsquid(){
+    var img= document.getElementById("imagesquid")
+    var div1=document.getElementById("squid1Ani")
+    var div2=document.getElementById("squid2Ani")
+    var div3 = document.getElementById("squid3Ani")
+    var div4 = document.getElementById("squid4Ani")
     if(img.style.display && div1.style.display == "block"){
         img.style.display="none"
         div1.style.display="none"
@@ -64,14 +64,14 @@ function showBeetle(){
     
 }
 
-function hideBeetle(){
-    var img= document.getElementById("imageBeetle")
-    var div1=document.getElementById("beetle1Ani")
+function hidesquid(){
+    var img= document.getElementById("imagesquid")
+    var div1=document.getElementById("squid1Ani")
     var imgSetting = img.style.display
     var div1Setting = div1.style.display
-    var div2=document.getElementById("beetle2Ani")
-    var div3=document.getElementById("beetle3Ani")
-    var div4=document.getElementById("beetle4Ani")
+    var div2=document.getElementById("squid2Ani")
+    var div3=document.getElementById("squid3Ani")
+    var div4=document.getElementById("squid4Ani")
     if(imgSetting && div1Setting == "none"){
         img.style.display="block"
         div1.style.display="block"
