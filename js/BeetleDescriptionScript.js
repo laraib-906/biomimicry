57005b94eb8a995092d481a6fcad6be868992749
laraib-906 <56772304+laraib-906@users.noreply.github.com>
2020-07-29 00:23:24 +0500

    internal pages of quid, snail, bettle and about us page

---
+++ js/BeetleDescriptionScript.js
@@ -0,0 +1,10 @@
+function moveBeetleMap(){
+    var imagMap = document.getElementById("beetleMap");
+    imagMap.src="../../media/image/Maps/Beetle/Beetle_01.png"
+    
+}
+function stopBeetleMap(){
+    var imagMap = document.getElementById("beetleMap");
+    imagMap.src="../../media/image/Maps/Beetle/Beetle_02.png"
+    
+}
---
+++ js/ShellDescriptionScript.js
@@ -0,0 +1,10 @@
+function moveShellMap(){
+    var imagMap = document.getElementById("shellMap");
+    imagMap.src="../../media/image/Maps/Shell/Shell_01.png"
+    
+}
+function stopShellMap(){
+    var imagMap = document.getElementById("shellMap");
+    imagMap.src="../../media/image/Maps/Shell/Shell_02.png"
+    
+}
